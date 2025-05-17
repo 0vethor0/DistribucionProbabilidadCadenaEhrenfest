dfa70f9a93935f8a39262ccd0e27a3795489d128
0vethor0 <161180971+0vethor0@users.noreply.github.com>
2025-05-16 22:57:14 -0400

    Carga inicial

---
+++ app/DistribucionProbabilidadCadenaEhrenfest/src/distribucionprobabilidadcadenaehrenfest/DistribucionProbabilidadCadenaEhrenfest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package distribucionprobabilidadcadenaehrenfest;
+
+/**
+ *
+ * @author Personal
+ */
+public class DistribucionProbabilidadCadenaEhrenfest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrmDistribucionEhrenfest form = new JFrmDistribucionEhrenfest();
+        form.setVisible(true);
+    }
+    
+}
